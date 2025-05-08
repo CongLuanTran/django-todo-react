69cd8f11155cf5dd9eab607815e3877958b6fda1
CongLuanTran <congluan.tran19@gmail.com>
2025-05-08 19:58:55 +0700

    fix env variable reading

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,10 +21,10 @@ env.read_env()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["192.168.1.107", "localhost", "127.0.0.1"]
 
