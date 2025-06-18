c299b6f7dbd32e7e1c227466249cfb00e327af53
Cong Luan Tran <congluan.tran19@gmail.com>
2025-06-18 23:16:29 +0700

    fix: delegate ALLOWED_HOSTS to dotenv file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["192.168.1.107", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS: list[str] = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
