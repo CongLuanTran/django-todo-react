3188db15d40b7d0674173b74012a5f2d3a5bb9ed
Cong Luan Tran <congluan.tran19@gmail.com>
2025-06-19 00:01:21 +0700

    feat: use Whitenoise to make the backend serve its own staticfiles

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# --------- Build the frontend static files ---------
 FROM oven/bun:alpine AS frontend-builder
 
 WORKDIR /app
@@ -6,11 +7,10 @@ WORKDIR /app/frontend
 
 RUN bun install && bun run build
 
+# --------- Build the Nginx image ---------
 FROM nginx:alpine
 
 COPY ./nginx/nginx.conf /etc/nginx/conf.d/default.conf
-COPY ./backend/staticfiles/ usr/share/nginx/html/backend_static
-
 COPY --from=frontend-builder /app/frontend/dist/ /usr/share/nginx/html/frontend/
 
 EXPOSE 80
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,11 +15,11 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+_ = env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -119,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
