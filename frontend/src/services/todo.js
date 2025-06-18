57b833bf6b86e49e1f2c4e7d36436756ac0777cc
Cong Luan Tran <congluan.tran19@gmail.com>
2025-06-18 23:18:18 +0700

    fix: use relative path instead of hardcoded

--- frontend/src/services/todo.js
+++ frontend/src/services/todo.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = `http://${import.meta.env.VITE_API_URL}/api/todos/`
+const baseUrl = '/api/todos/'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
